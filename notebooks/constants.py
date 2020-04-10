f25137df61f468eb35040f5443ea033ad8430476
julian-west <47723873+julian-west@users.noreply.github.com>
2020-04-10 22:03:16 +0100

    tidy

---
+++ notebooks/constants.py
@@ -0,0 +1,9 @@
+"""
+File to define constants
+"""
+
+DATA_FOLDER = "../data"
+VISUALISATION_FOLDER = "../report/visualisations"
+RESULTS_FOLDER = "../report/results"
+BRAND_COLORS = ["#0071bc", "#e7b800", "#d4145a"]
+
